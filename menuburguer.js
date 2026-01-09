ecf52292ff13a07e9b6f66c63b6ffaa3834bb36a
Miguelpedr222 <joaomiguelalvespedrosa@gmail.com>
2026-01-09 17:00:39 -0300

    Add burgerbtn function to toggle menu visibility

---
+++ menuburguer.js
@@ -0,0 +1,7 @@
+function burgerbtn() {
+  const menu = document.getElementById("menu");
+  menu.classList.toggle("menu-aberto");
+}function burgerbtn() {
+  const menu = document.getElementById("menu");
+  menu.classList.toggle("menu-aberto");
+}
